9901b712a4c7f99517681cd20fb42d828aacebae
Sergio <salanitri@gmail.com>
2020-09-16 20:00:43 -0300

    additional files and changes for Heroku

--- primeslab/settings/base.py
+++ primeslab/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import django_heroku
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
@@ -162,3 +163,6 @@ WAGTAIL_SITE_NAME = "primeslab"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
